b0ae593f095079e94daa181893be60979c338df5
Andrij <andrey.m.savchuk@gmail.com>
2019-05-16 17:47:09 +0300

    0000

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Enhancer from './Enhancer';
-import ButtonComponent from './Button.component';
+import Enhancer from "./Enhancer";
+import ButtonComponent from "./Button.component";
 
 import "./styles.css";
 
